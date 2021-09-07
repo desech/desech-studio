c2777ec337197c519840cbe16bef19443488e32f
catalin <catalin.luntraru@desech.com>
2021-09-07 13:00:05 +0300

    fix transition on temp styles

--- src/js/state/StateTempStyle.js
+++ src/js/state/StateTempStyle.js
@@ -5,23 +5,28 @@ import CanvasCommon from '../main/canvas/CanvasCommon.js'
 import HelperDOM from '../helper/HelperDOM.js'
 
 export default {
+  setStyleValue (property, value) {
+    this.setStyles({ [property]: value })
+  },
+
   setStyles (properties) {
+    this.addStyles(properties)
+    CanvasCommon.hideElementOverlay(Object.keys(properties))
+  },
+
+  addStyles (properties) {
     const element = StateSelectedElement.getElement()
     for (const [name, value] of Object.entries(properties)) {
       element.style[name] = value
     }
-    CanvasCommon.hideElementOverlay(Object.keys(properties))
-  },
-
-  setStyleValue (property, value) {
-    const element = StateSelectedElement.getElement()
-    element.style[property] = value
-    CanvasCommon.hideElementOverlay([property])
+    // make sure transitions are disabled
+    element.style['transition-property'] = 'none'
   },
 
   applyStyleValue (panelReload = false, ignoreZeroValues = false) {
     const element = StateSelectedElement.getElement()
     const style = HelperStyle.getInlineStyle(element, ignoreZeroValues)
+    if (style['transition-property']) delete style['transition-property']
     HelperDOM.clearStyle(element)
     RightCommon.changeStyle(style, panelReload)
   }
